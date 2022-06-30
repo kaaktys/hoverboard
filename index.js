dd25b5f61f691415b8dc981c34280c92e99a8e44
Kate <katok0709@gmail.com>
2022-07-01 01:32:12 +0300

    js + finished css

---
+++ index.js
@@ -0,0 +1,29 @@
+const wrapper = document.getElementById('wrapper')
+const colors = ['#e73cae', '#aa51d0', '#5eafe6', '#b9e841', '#34cd4e']
+const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    wrapper.appendChild(square)
+}
+
+function setColor(element) {
+   const color = getRandomColor()
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
